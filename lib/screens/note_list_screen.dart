e2d9dfe677dbf6997a97e8fb1642320788b74259
rizkiambarwati <rizkiambarwati_2226250079@mhs.mdp.ac.id>
2024-04-16 09:40:07 +0700

    notes

---
+++ lib/screens/note_list_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class NoteListScreen extends StatefulWidget {
+  const NoteListScreen({super.key});
+
+  @override
+  State<NoteListScreen> createState() => _NoteListScreenState();
+}
+
+class _NoteListScreenState extends State<NoteListScreen> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      body: const Placeholder(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Add"),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Text(
+                        'Title: ',
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    TextField(
+                      controller: _titleController,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Text(
+                        'Description: ',
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    TextField(
+                      controller: _descriptionController,
+                    ),
+                  ],
+                ),
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: const Text("Save"))
+                ],
+              );
+            },
+          );
+        },
+        tooltip: 'Add Notes',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
